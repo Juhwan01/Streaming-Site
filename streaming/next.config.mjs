aadad4fdf59a0f522c3b26340bf98bdb2a9cf3fe
juhwan <127858798+Juhwan01@users.noreply.github.com>
2024-11-27 10:13:57 +0900

    Update next.config.mjs

--- streaming/next.config.mjs
+++ streaming/next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['실제 사용할 도메인']
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://3.36.103.8:8001/:path*'
+      }
+    ]
+  }
 }
 
 export default nextConfig;
