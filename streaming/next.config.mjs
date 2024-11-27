c0320f7a5460e6d8c533c5222f1300c8434da48a
Juhwan01 <wnghks5432@naver.com>
2024-11-27 11:45:06 +0900

    점검5

--- streaming/next.config.mjs
+++ streaming/next.config.mjs
@@ -19,26 +19,7 @@ const nextConfig = {
       }
     ]
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.iamport.kr",
-              "connect-src 'self' ws: wss: http: https:",
-              "img-src 'self' blob: data:",
-              "media-src 'self' blob:",
-              "upgrade-insecure-requests",
-            ].join('; ')
-          }
-        ]
-      }
-    ]
-  }
+  
 }
 
 export default nextConfig;
